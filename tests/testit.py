4fbca6c2adb6746c28688075c4d69f00f018e72f
Mehmet GÖK <mehmetgok@Mehmet-MacBook-Pro.local>
2019-05-06 03:00:55 +0300

    initial commit

---
+++ tests/testit.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+
+
+class RunChromeTests():
+    # http://chromedriver.storage.googleapis.com/index.html
+
+    def test(self):
+        driverLocation = "/Users/mehmetgok/Documents/drivers/chromedriver_mac64"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        # Instantiate Chrome Browser Command
+        driver = webdriver.Chrome(driverLocation)
+        # Open the provided URL
+        driver.get("http://localhost:8000/lucky/number")
+        driver.implicitly_wait(10)
+        # id next
+        whatsNext = driver.find_element(By.ID, "num")
+        print(whatsNext.text)
+        assert 0 <= int(whatsNext.text) <= 100
+
+
+ff = RunChromeTests()
+ff.test()
